4f868be40e19d2ace2d7329853b6b38a3a6c253b
kevinQTruong <kevinQTruong@users.noreply.github.com>
2023-06-26 08:31:20 +1000

    day 25

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import time as tm
 from datetime import time, datetime
 import numpy as np
 import altair as alt
@@ -6,6 +7,144 @@ import streamlit as st
 from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(layout="wide")
+
+st.header("session state - Day 25")
+
+
+def lbs_to_kg():
+    st.session_state.kg = st.session_state.lbs / 2.2046
+
+
+def kg_to_lbs():
+    st.session_state.lbs = st.session_state.kg * 2.2046
+
+
+st.header("Input")
+col1, spacer, col2 = st.columns([2, 1, 2])
+with col1:
+    pounds = st.number_input("Pounds:", key="lbs")
+
+with col2:
+    # by giving a key "kg" streamlit will auto store this value int the session state
+    kilogram = st.number_input("Kilograms:", key="kg", on_change=kg_to_lbs)
+
+st.header("Output")
+st.write("st.session_state object:", st.session_state)
+
+
+st.header("st.cache - Day 24")
+
+# Using cache
+a0 = tm.time()
+st.subheader("Using st.cache")
+
+
+@st.cache_data
+def load_data_a():
+    df = pd.DataFrame(np.random.rand(2000000, 5), columns=["a", "b", "c", "d", "e"])
+    return df
+
+
+st.write(load_data_a())
+a1 = tm.time()
+st.info(a1 - a0)
+
+# Not using cache
+b0 = tm.time()
+st.subheader("Not using st.cache")
+
+
+def load_data_b():
+    df = pd.DataFrame(np.random.rand(4000000, 5), columns=["a", "b", "c", "d", "e"])
+    return df
+
+
+st.write(load_data_b())
+b1 = tm.time()
+st.info(b1 - b0)
+
+
+st.header("st.experimental_get_query_params - Day 23")
+
+with st.expander("About this app"):
+    st.write(
+        "`st.experimental_get_query_params` allows the retrieval of query parameters directly from the URL of the user's browser."
+    )
+
+st.header("1. Instructions")
+st.markdown(
+    """
+In the above URL bar of your internet browser, append the following:
+`?firstname=Jack&surname=Beanstalk`
+after the base URL `http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/`
+such that it becomes 
+`http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/?firstname=Jack&surname=Beanstalk`
+"""
+)
+
+# 2. Contents of st.experimental_get_query_params
+st.header("2. Contents of st.experimental_get_query_params")
+st.write(st.experimental_get_query_params())
+
+st.header("3. Retrieving and displaying information from the URL")
+query_params = st.experimental_get_query_params()
+
+firstname = query_params.get("firstname", [""])[0]
+surname = query_params.get("surname", [""])[0]
+
+st.write(f"Hello **{firstname} {surname}**, how are you?")
+
+st.header("form - Day 22")
+
+st.header("1. Example of using `with` notation")
+st.subheader("Coffee machine")
+
+with st.form("my_form"):
+    st.subheader("**Order your coffee**")
+
+    # Input widgets
+    coffee_bean_val = st.selectbox("Coffee bean", ["Arabica", "Robusta"])
+    coffee_roast_val = st.selectbox("Coffee roast", ["Light", "Medium", "Dark"])
+    brewing_val = st.selectbox(
+        "Brewing method", ["Aeropress", "Drip", "French press", "Moka pot", "Siphon"]
+    )
+    serving_type_val = st.selectbox("Serving format", ["Hot", "Iced", "Frappe"])
+    milk_val = st.select_slider("Milk intensity", ["None", "Low", "Medium", "High"])
+    owncup_val = st.checkbox("Bring own cup")
+
+    # Every form must have a submit button
+    submitted = st.form_submit_button("Submit")
+
+if submitted:
+    st.markdown(
+        f"""
+        ☕ You have ordered:
+        - Coffee bean: `{coffee_bean_val}`
+        - Coffee roast: `{coffee_roast_val}`
+        - Brewing: `{brewing_val}`
+        - Serving type: `{serving_type_val}`
+        - Milk: `{milk_val}`
+        - Bring own cup: `{owncup_val}`
+        """
+    )
+else:
+    st.write("☝️ Place your order!")
+
+
+st.header("progress - Day 21")
+
+with st.expander("About this app"):
+    st.write(
+        "You can now display the progress of your calculations in a Streamlit app with the `st.progress` command."
+    )
+
+my_bar = st.progress(0)
+
+for percent_complete in range(100):
+    tm.sleep(0.05)
+    my_bar.progress(percent_complete + 1)
+
+st.balloons()
 
 st.header("Layout - Day 19")
 
@@ -24,29 +163,32 @@ user_name = st.sidebar.text_input("What is your name?")
 user_emoji = st.sidebar.selectbox(
     "Choose an emoji", ["", "😄", "😆", "😊", "😍", "😴", "😕", "😱"]
 )
-user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
+user_food = st.sidebar.selectbox(
+    "What is your favorite food?",
+    ["", "Tom Yum Kung", "Burrito", "Lasagna", "Hamburger", "Pizza"],
+)
 
-st.subheader('Output')
+st.subheader("Output")
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    if user_name != '':
-        st.write(f'👋 Hello {user_name}!')
+    if user_name != "":
+        st.write(f"👋 Hello {user_name}!")
     else:
-        st.write('👈  Please enter your **name**!')
+        st.write("👈  Please enter your **name**!")
 
 with col2:
-  if user_emoji != '':
-    st.write(f'{user_emoji} is your favorite **emoji**!')
-  else:
-    st.write('👈 Please choose an **emoji**!')
+    if user_emoji != "":
+        st.write(f"{user_emoji} is your favorite **emoji**!")
+    else:
+        st.write("👈 Please choose an **emoji**!")
 
 with col3:
-  if user_food != '':
-    st.write(f'🍴 **{user_food}** is your favorite **food**!')
-  else:
-    st.write('👈 Please choose your favorite **food**!')
+    if user_food != "":
+        st.write(f"🍴 **{user_food}** is your favorite **food**!")
+    else:
+        st.write("👈 Please choose your favorite **food**!")
 
 
 st.header("File uploader - Day 18")
