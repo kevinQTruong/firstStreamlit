58ef026322ea9eee35f8e50580a38c23e1a5defa
kevinQTruong <kevinQTruong@users.noreply.github.com>
2023-06-28 21:29:11 +1000

    day 27 streamlit element

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,119 @@ import pandas as pd
 import streamlit as st
 from streamlit_pandas_profiling import st_profile_report
 import requests
+import json
+from pathlib import Path
+
+from streamlit_elements import elements, dashboard, mui, editor, media, lazy, sync, nivo
 
 st.set_page_config(layout="wide")
+
+st.header("streamlit elements 3rd party - Day 27")
+
+with st.sidebar:
+    st.title("🗓️ #30DaysOfStreamlit")
+    st.header("Day 27 - Streamlit Elements")
+    st.write("Build a draggable and resizable dashboard with Streamlit Elements.")
+    st.write("---")
+
+    # Define URL for media player.
+    media_url = st.text_input(
+        "Media URL", value="https://www.youtube.com/watch?v=vIQQR_yq-8I"
+    )
+
+# Initialize default data for code editor and chart.
+#
+# For this tutorial, we will need data for a Nivo Bump chart.
+# You can get random data there, in tab 'data': https://nivo.rocks/bump/
+#
+# As you will see below, this session state item will be updated when our
+# code editor change, and it will be read by Nivo Bump chart to draw the data.
+
+if "data" not in st.session_state:
+    st.session_state.data = Path("data.json").read_text()
+
+# Define a default dashboard layout.
+# Dashboard grid has 12 columns by default.
+#
+# For more information on available parameters:
+# https://github.com/react-grid-layout/react-grid-layout#grid-item-props
+
+layout = [
+    # Editor item is positioned in coordinates x=0 and y=0, and takes 6/12 columns and has a height of 3.
+    dashboard.Item("editor", 0, 0, 6, 4),
+    # Chart item is positioned in coordinates x=6 and y=0, and takes 6/12 columns and has a height of 3.
+    dashboard.Item("chart", 6, 0, 6, 3),
+    # Media item is positioned in coordinates x=0 and y=3, and takes 6/12 columns and has a height of 4.
+    dashboard.Item("media", 0, 2, 12, 6),
+]
+
+with elements("demo"):
+    with dashboard.Grid(layout, draggableHandle=".draggable"):
+        with mui.Card(key="editor", sx={"display": "flex", "flexDirection": "column"}):
+            mui.CardHeader(title="Editor", className="draggable")
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+                editor.Monaco(
+                    defaultValue=st.session_state.data,
+                    language="json",
+                    onChange=lazy(sync("data")),
+                )
+            with mui.CardActions:
+                mui.Button("Apply changes", onClick=sync())
+
+        with mui.Card(key="chart", sx={"display": "flex", "flexDirection": "column"}):
+            mui.CardHeader(title="Chart", className="draggable")
+
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+                nivo.Bump(
+                    data=json.loads(st.session_state.data),
+                    colors={"scheme": "spectral"},
+                    lineWidth=3,
+                    activeLineWidth=6,
+                    inactiveLineWidth=3,
+                    inactiveOpacity=0.15,
+                    pointSize=10,
+                    activePointSize=16,
+                    inactivePointSize=0,
+                    pointColor={"theme": "background"},
+                    pointBorderWidth=3,
+                    activePointBorderWidth=3,
+                    pointBorderColor={"from": "serie.color"},
+                    axisTop={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "",
+                        "legendPosition": "middle",
+                        "legendOffset": -36,
+                    },
+                    axisBottom={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "",
+                        "legendPosition": "middle",
+                        "legendOffset": 32,
+                    },
+                    axisLeft={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "ranking",
+                        "legendPosition": "middle",
+                        "legendOffset": -40,
+                    },
+                    margin={"top": 40, "right": 100, "bottom": 40, "left": 60},
+                    axisRight=None,
+                )
+
+            with mui.CardActions:
+                mui.Button("Apply changes", onClick=sync())
+
+        with mui.Card(key="media", sx={"display": "flex", "flexDirection": "column"}):
+            mui.CardHeader(title="Media Player", className="draggable")
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+                media.Player(url=media_url, width="100%", height="100%", controls=True)
+
 
 st.header("external api - Day 26")
 st.sidebar.header("Input")
